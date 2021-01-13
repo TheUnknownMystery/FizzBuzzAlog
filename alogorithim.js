3ad2658b2828228fe2bb105b330ee2e8f3ab495b
sauhardo2020 <70318077+sauhardo2020@users.noreply.github.com>
2021-01-13 09:13:16 +0530

    Create alogorithim.js

---
+++ alogorithim.js
@@ -0,0 +1,24 @@
+//to create a fizz buzz alogrithim we must know what it does
+//when a number is divided by 5 or and other no dived by 5 which gives a reminder of 0
+
+var num = 100 //example number
+
+function BizzFuzzAlog(n){
+
+if(n % 5 === 0 )
+{
+
+console.log(n + " this is a fizz number")
+
+
+  }
+
+else if(n % 3 === 0){
+
+console.log(n + 'this is a buzz number')
+
+
+ }
+
+
+}
